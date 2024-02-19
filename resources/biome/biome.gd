bc7ff02925d8c15a5f6f89640147df76f6842429
seether21 <21codyperry@gmail.com>
2024-02-19 16:31:59 -0600

    changes to biome resource creation

--- resources/biome/biome.gd
+++ resources/biome/biome.gd
@@ -1,9 +1,21 @@
 extends Resource
 class_name Biome
 
+#Biome Details
+@export var biome_name: String
 @export var percentage_chance: float = 0
 @export var connecting_biomes: Array[Biome]
-@export var grass_chance: float = 0
-@export var grass_atlas_id: int = 0
-@export var grass_atlas_index: Vector2i = Vector2i.ZERO
+
+#Generation Details
+@export var atlas_id: int
+@export var atlas_inded: Vector2i
+@export var terrain_id: int
+@export var terrain: int
+@export var layer: int
+
+#Vegetation Details
+@export var vegetation_chance: float = 0
+@export var vegetation_atlas_id: Array[int] = [0]
+@export var vegetation_atlas_index: Array[Vector2i] = [Vector2i.ZERO]
+
 
